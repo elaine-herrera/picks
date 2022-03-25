e0eab3ff5b68d95cf1f8547b6d084c117b626372
Elaine <elainehdelcueto@gmail.com>
2022-03-25 16:02:13 -0400

    Added a video details router

---
+++ picks/Video Module/VideoRouter.swift
@@ -0,0 +1,14 @@
+//
+//  VideoRouter.swift
+//  picks
+//
+//  Created by Elaine Herrera on 25/3/22.
+//
+
+import SwiftUI
+
+class VideoRouter {
+    func makeVideoDetailView(for video: Video, model: DataModel) -> some View {
+        return EmptyView()
+    }
+}
