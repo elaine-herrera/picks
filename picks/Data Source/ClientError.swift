d02c43889d578e922a077069b5a174aaa8308b5c
Elaine <elainehdelcueto@gmail.com>
2022-03-28 12:21:25 -0400

    Added error description

--- picks/Data Source/ClientError.swift
+++ picks/Data Source/ClientError.swift
@@ -13,3 +13,18 @@ enum ClientError: Error {
     case invalidAccessToken
     case anyError
 }
+
+extension ClientError: LocalizedError{
+    public var errorDescription: String?{
+        switch self {
+        case .missingApiConfig:
+            return "Missing Api Configutation"
+        case .invalidServerResponse:
+            return "Invalid Server Response"
+        case .invalidAccessToken:
+            return "Invalid Access Token"
+        case .anyError:
+            return "Unknown error"
+        }
+    }
+}
